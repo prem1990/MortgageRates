9004b00f26dc83a1ca5bfa2be73dcdcd7c4c8803
ValiOrBali <valibali9009@gmail.com>
2025-09-19 14:15:46 -0400

    updated scripts

--- scrape_rates.sh
+++ scrape_rates.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/bash
-source_file=""
-curl_output="curl_output.txt"
-
+source_file="$(dirname $(readlink -f $0))/credit_union.csv"
+curl_output="$(dirname $(readlink -f $0))/curl_output.txt"
+wget_output="$(dirname $(readlink -f $0))/credit_units.txt"
+base_url="https://mortgages.cumortgage.net/start_up.asp"
+siteid_url="https://mortgages.cumortgage.net/default.asp?siteId="
 
 get_mortgage_units()
 {
-asdasd
+[[ -n start_up.asp ]] && rm -rf start_up.asp*
+wget "${base_url}"
+[[ -n start_up.asp ]] && cat start_up.asp| grep "option value="| grep -v 'option value="' > "${wget_output}"
+for i in $(cat "${wget_output}"  |sed 's/> </>\n</g'|sed 's/<option //g;s/<\/option>//g;s/value=//g;s/ /-/g')
+do 
+sid=$(echo "${i}"|cut -d'>' -f1)
+union_name=$(echo "${i}"|cut -d">" -f2|sed 's/-//g'|sed "s/,//g;s/\.//g;s/'//g"|sed 's/\r//g')
+echo "$union_name,$siteid_url$sid"
+done > "${source_file}"
 }
-
-
-
 
 get_mortgage_rates()
 {
@@ -20,12 +27,15 @@ do
 	union_name=$(echo "${single_union}"|cut -d, -f1)
 	union_link=$(echo "${single_union}"|cut -d, -f2)
 	curl -s $union_link > $curl_output
-	#output=$(grep 'border="0"' $curl_output  | grep Today | sed 's/></>\n</g' |grep -E "productDetailsSamplePmt|Interest Rate"|sed 's/<\/a>//g;s/<\/td>//g'|rev|cut -d">" -f1|rev|sed 's/ //g' | paste -sd','|sed 's/Conforming,/Conforming|/g;s/Conforming1,/Conforming|/g;s/ARM,/ARM|/g')
+	while ! grep -q "Today" $curl_output; do
+ 	curl -s $union_link > $curl_output
+	sleep 5 # Wait for 5 seconds before checking again
+	done
 	output=$(grep 'border="0"' $curl_output  | grep Today | sed 's/></>\n</g' |grep -E "productDetailsSamplePmt|Interest Rate"|sed 's/<\/a>//g;s/<\/td>//g'|rev|cut -d">" -f1|rev|sed 's/ //g'|sed 's/-/|/g' |paste -sd','|sed 's/|/-/g'|sed 's/%,/%|/g;s/,/#/g;s/|/,/g;s/#/|/g')
 	if [[ -z "${output}" ]]; then
 	output=$(cat $curl_output  | grep Today | sed 's/></>\n</g' |grep -E "productDetailsSamplePmt|Interest Rate"|sed 's/<\/a>//g;s/<\/td>//g'|rev|cut -d">" -f1|rev|sed 's/ //g'|sed 's/-/|/g' |paste -sd','|sed 's/|/-/g'|sed 's/%,/%|/g;s/,/#/g;s/|/,/g;s/#/|/g')	
-	#output=$(cat $curl_output  | grep Today | sed 's/></>\n</g' |grep -E "productDetailsSamplePmt|Interest Rate"|sed 's/<\/a>//g;s/<\/td>//g'|rev|cut -d">" -f1|rev|sed 's/ //g' | paste -sd','|sed 's/Conforming,/Conforming|/g;s/Conforming1,/Conforming|/g;s/ARM,/ARM|/g')
 	fi
+
 	for single_output in $(echo "${output}"|sed 's/,/\n/g')
 	do
 		mortgage_program=$(echo "${single_output}"|cut -d"|" -f1|sed 's/-Conforming//g;s/-Conforming1//g')
@@ -41,3 +51,8 @@ do
         echo "$union_name,$union_link,$rate_output"|sed 's/.$//g'
 done
 }
+
+#get_mortgage_units
+#get_mortgage_rates
+rm -rf start_up.asp*
+rm -rf $curl_output
